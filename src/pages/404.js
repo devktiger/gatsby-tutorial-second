66c28e4885fc64e2e8acf71b77a2a71f72a1662e
Andrew <dev.andrewhan@gmail.com>
2019-11-07 22:26:42 -0500

    env variables added

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Layout from '../component/layout';
+const Error = () => {
+	return <Layout>this is our error page</Layout>;
+};
+
+export default Error;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
-import React from "react"
-
-export default () => <div>Hello world!</div>
+// global css
+// miultiple elements
+import React from 'react';
+import Layout from '../component/layout';
+import { Button } from '../component/button';
+export default () => (
+	<Layout>
+		<h1 className='title'>this is our home page</h1>
+		<h1 className='title'>another heading</h1>
+		Hello world!
+		<h2>this second heading</h2>
+		<Button>styled button</Button>
+	</Layout>
+);
